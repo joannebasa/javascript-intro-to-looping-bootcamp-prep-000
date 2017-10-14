06ef717c0d4a32acef9fd22d203c566d650aeab5
Joanne Basa <joannegbasa@gmail.com>
2017-10-14 08:12:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,38 @@
-var array = [];
+function forLoop (array) {
+  for (let i = 0; i < 25; i++)
 
-function forLoop(array) {
-  for (let n = 0; n < 25; n++) {
-    if (n === 1) {
-      array.push("I am 1 strange loop");
-    } else {
-      return array;
-    }
-  }
+  if (i === 1 ) {
+
+    array.push ("I am 1 strange loop");
+
+  } else {
+
+  array.push (`I am ${i} strange loops`);
+ }
+
+ return array;
 }
+
+
+function whileLoop(n) {
+
+  while (n > 0) {
+    console.log (--n);
+  }
+
+  return 'done';
+}
+
+
+ function doWhileLoop(array) {
+
+   function maybeTrue() {
+     return Math.random() >= 0.5;
+}
+
+  do {
+    array.pop();
+} while (array.length > 0 && maybeTrue());
+
+  return array;
+ }
