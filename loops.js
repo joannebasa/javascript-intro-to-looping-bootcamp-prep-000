3ef98deeea45ad10d91e2b5bb58a67fe64843099
Joanne Basa <joannegbasa@gmail.com>
2017-10-14 08:10:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 var array = [];
 
-function for loop(array) {
+
   for (let n = 0; n < 25; n++) {
     if (n = 1) {
       add "I am 1 strange loop";
@@ -8,4 +8,3 @@ function for loop(array) {
       return array;
     }
   }
-}
